f4a98b529e27dfec8c7afc6feda0e6c2552466da
xWsoRa <130982325+xWsoRa@users.noreply.github.com>
2023-04-17 22:55:22 +0200

    Update script_menu.js

--- script_menu.js
+++ script_menu.js
@@ -4,9 +4,9 @@ gameButton1.addEventListener("click", function() {
 });
 const gameButton2 = document.getElementById("gameButton2");
 gameButton2.addEventListener("click", function() {
-  window.open("https://xwsora.github.io/Fill_Blank/fill_gap", "_blank");
+  window.open("https://xwsora.github.io/fill_Blank_final/", "_blank");
 });
 const gameButton3 = document.getElementById("gameButton3");
 gameButton3.addEventListener("click", function() {
-  window.open("https://xwsora.github.io/fill_Blank_final/", "_blank");
+  window.open("https://xwsora.github.io/long_questions/", "_blank");
 });
