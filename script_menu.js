e6a35cb83b703e07fd5341c7f447905b4da801f0
xWsoRa <130982325+xWsoRa@users.noreply.github.com>
2023-04-17 22:42:50 +0200

    Add files via upload

---
+++ script_menu.js
@@ -0,0 +1,12 @@
+const gameButton1 = document.getElementById("gameButton1");
+gameButton1.addEventListener("click", function() {
+  window.open("https://xwsora.github.io/Multiple-choice/", "_blank");
+});
+const gameButton2 = document.getElementById("gameButton2");
+gameButton2.addEventListener("click", function() {
+  window.open("https://xwsora.github.io/Fill_Blank/fill_gap", "_blank");
+});
+const gameButton3 = document.getElementById("gameButton3");
+gameButton3.addEventListener("click", function() {
+  window.open("https://xwsora.github.io/long_questions/", "_blank");
+});
